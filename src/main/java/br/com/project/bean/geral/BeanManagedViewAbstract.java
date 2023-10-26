8d5f2e16bdc8fe934ee425943438a74c14d8eda9
lucasgodoyy <ehlucasgodoy10@gmail.com>
2023-10-26 10:09:25 -0300

    LoginBeanView

---
+++ src/main/java/br/com/project/bean/geral/BeanManagedViewAbstract.java
@@ -0,0 +1,21 @@
+package br.com.project.bean.geral;
+
+import org.springframework.stereotype.Component;
+
+import br.com.project.report.util.BeanReportView;
+
+/**
+ * Responsavel pela rotina de consulta e abstracao de metodos de CRUD e outros
+ * padroes
+ * 
+ * @author alex
+ * 
+ */
+@Component
+public abstract class BeanManagedViewAbstract extends BeanReportView {
+
+	private static final long serialVersionUID = 1L;
+
+
+}
+
