a2468fa9209a5800d69886fa4fd5453c47a24127
lucasgodoyy <ehlucasgodoy10@gmail.com>
2023-11-03 09:12:07 -0300

    Classes Cidade

---
+++ src/main/java/br/com/project/bean/view/CidadeBeanView.java
@@ -0,0 +1,34 @@
+package br.com.project.bean.view;
+
+import javax.faces.bean.ManagedBean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import br.com.project.bean.geral.BeanManagedViewAbstract;
+import br.com.project.geral.controller.CidadeController;
+
+
+@Controller
+@Scope("session")
+@ManagedBean(name = "cidadeBeanView")
+public class CidadeBeanView extends BeanManagedViewAbstract{
+
+	
+	private static final long serialVersionUID = 1L;
+
+	@Autowired
+	private CidadeController cidadeController;
+	
+	
+	public void setCidadeController(CidadeController cidadeController) {
+		this.cidadeController = cidadeController;
+	}
+
+	public CidadeController getCidadeController() {
+		return cidadeController;
+	}
+	
+	
+}
